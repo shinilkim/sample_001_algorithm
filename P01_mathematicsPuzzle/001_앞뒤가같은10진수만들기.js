c8ecff2666ef48dbfbd19fbfb5486163ec81cae5
shinil.kim <shinil.kim@hotmail.com>
2018-08-09 11:01:14 +0900

    Update 001_앞뒤가같은10진수만들기.js

--- P01_mathematicsPuzzle/001_앞뒤가같은10진수만들기.js
+++ P01_mathematicsPuzzle/001_앞뒤가같은10진수만들기.js
@@ -1,3 +1,11 @@
+/*
+# 001 앞뒤가 같은 10진수 만들기
+
+10진수, 2진수, 8진수 그 어느 것으로 표현하여도 대칭수가 되는 수중,
+10진수에서 10 이상인 최소값을 구해 보세요.
+대칭수: 앞뒤가 같아 거꾸로 읽어도 같은 수 (예: 11)
+*/
+
 /* 문자열 형식으로 역순을 반환하는 메소드를 추가 */
 String.prototype.reverse = function() {
 	return this.split("").reverse().join("");
@@ -11,9 +19,9 @@ while (true)
 		(num.toString(8) == num.toString(8).reverse()) &&
 		(num.toString(2) == num.toString(2).reverse())) {
 
-		document.write('10진수: '+num+'<br/>');
-		document.write('8진수: '+num.toString(8)+'<br/>');
-		document.write('2진수: '+num.toString(2));
+		console.log('10진수: '+num);
+		console.log('8진수: '+num.toString(8));
+		console.log('2진수: '+num.toString(2));
 		break;
 	}
 	num += 2;
